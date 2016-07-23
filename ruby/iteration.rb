c87989a93a18e4018bfeceb8c0c7e95b7f4caafa
omgannie <annie_oh@icloud.com>
2016-07-23 16:50:53 -0500

    5.3 Release 2 complete

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -13,3 +13,77 @@ books.each do |book, status|
     p "This book has not read yet."
   end
 end
+
+
+movies = {
+	finding_dory: 5,
+	movie_2: 7,
+	movie_3: 9
+}
+
+actors = ["Ellen Paige", "Hugh Jackman", "Patrick Stewart"]
+
+#presenting the original rating of each movie
+movies.each do |movie, rating|
+	puts "The movie #{movie.to_s} has a rating of #{rating}"
+end
+
+#presenting the original actor list
+actors.each do |actor|
+	puts "The actor #{actor} has an index of #{actors.index(actor)}"
+end
+
+#jumble the actors' names
+actors.map! do |actor|
+	actor.reverse
+end
+
+#presenting the final rating of each movie
+movies.each do |movie, rating|
+	puts "The movie #{movie.to_s} has a rating of #{rating}"
+end
+
+#presenting the changed actor list
+actors.each do |actor|
+	puts "The actor #{actor} has an index of #{actors.index(actor)}"
+end
+
+# Release 2
+# method iterates thru items, deleting any that meets certain condition
+grocery_list = ["milk", "juice", "cookies"]
+
+dinner_menu = {
+  first_course: "soup",
+  second_course: "salad",
+  main_course: "steak",
+  dessert: "chocolate cake"
+}
+
+grocery_list.delete_if { |item| item.length < 5 }
+
+p grocery_list
+
+dinner_menu.delete_if { |course, meal| meal.length < 5 }
+
+p dinner_menu
+
+# method to filter a data structure for items to meet condition
+p grocery_list.select { |item| item.length == 5 }
+
+p dinner_menu.select { |course, meal| meal.length == 5 }
+
+# filter data structure, different method for meeting condition
+grocery_list.keep_if { |item| item.length > 5 }
+p grocery_list
+
+dinner_menu.keep_if { |course, meal| meal.length > 5 }
+p dinner_menu
+
+# method to remove items until condition
+# in block = false, then breaks
+
+grocery_list.reject! { |item| item == "cookies" }
+p grocery_list
+
+dinner_menu.reject! { |course, meal| meal == "chocolate cake" }
+p dinner_menu
