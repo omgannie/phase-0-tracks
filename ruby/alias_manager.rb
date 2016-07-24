33c52072149454aa2d672c9e9635c4b1df8f5320
omgannie <annie_oh@icloud.com>
2016-07-24 14:50:27 -0500

    5.5 Solo Initial commit

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,11 @@
+# Release 0
+# Method that accepts spy's real name and creates a fake name
+# 1. Swap first and last name
+# 2. Change all of the vowels to the next vowel in 'aeiou'
+# 3. All of the consonants would change to the next letter in the alphabet
+
+def alias_generator(full_name)
+  full_name.split(' ').reverse
+
+  
+end
