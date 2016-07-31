e2c2fa4c468ec771b17f2bcfb67d0ff82ec4d5c3
omgannie <annie_oh@icloud.com>
2016-07-31 14:46:10 -0500

    Release 1 add attributes to Santa, loop through genders and ethnicities to initialize multiple Santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,9 +1,14 @@
 # Release 0
 
 class Santa
-  def initalize
+  def initalize(gender, ethnicity)
     puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
   end
+
+  reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+  age = 0
 
   def speak
     puts "Ho ho ho! Haaappy holidays!"
@@ -15,6 +20,13 @@ class Santa
 end
 
 # Driver Code
-first_santa = Santa.new
+first_santa = Santa.new("Annie", "Korean")
 first_santa.speak
 first_santa.eat_milk_and_cookies("chocolate chip")
+
+santas = []
+genders = ["female", "male", "female", "gender fluid", "N/A"]
+ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+genders.length.times do |i|
+  santas << Santa.new(genders[i], ethnicities[i])
+end
