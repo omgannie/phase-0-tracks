e55d57cef0ba80e6949363e1d178d57f6d8e56d4
omgannie <annie_oh@icloud.com>
2016-07-31 15:41:27 -0500

    Release 2 complete - complete birthday, get mad at methods, getter and setter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,14 +1,11 @@
-# Release 0
-
 class Santa
-  def initalize(gender, ethnicity)
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance..."
     @gender = gender
     @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
-
-  reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-  age = 0
 
   def speak
     puts "Ho ho ho! Haaappy holidays!"
@@ -17,10 +14,41 @@ class Santa
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type} cookie!"
   end
+
+  def celebrate_birthday
+    @age += 1
+
+    p @age
+  end
+
+  def get_mad_at(reindeer_name)
+    @reindeer_ranking.each do |name|
+      if name == reindeer_name
+        @reindeer_ranking.delete(name)
+        @reindeer_ranking << name
+      end
+    end
+
+    p @reindeer_ranking
+  end
+
+# setter method
+  def gender=(new_gender)
+    @gender = new_gender
+  end
+
+# getter methods
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
 end
 
 # Driver Code
-first_santa = Santa.new("Annie", "Korean")
+first_santa = Santa.new("female", "Russian")
 first_santa.speak
 first_santa.eat_milk_and_cookies("chocolate chip")
 
@@ -30,3 +58,9 @@ ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to sa
 genders.length.times do |i|
   santas << Santa.new(genders[i], ethnicities[i])
 end
+
+first_santa.celebrate_birthday
+first_santa.get_mad_at("Donner")
+first_santa.gender = "intersex"
+first_santa.age
+first_santa.ethnicity
