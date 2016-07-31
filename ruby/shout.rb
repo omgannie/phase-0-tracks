3801927a8e84c1dbe6c3ad223880b513ab5899b4
omgannie <annie_oh@icloud.com>
2016-07-31 16:45:39 -0500

    Release 3 convert module to mixin with driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,36 @@
+#module Shout
+#  def self.yell_angrily(words)
+#    p words + "!!!" + " :("
+#  end
+
+#  def self.yell_happily(words)
+#    p words + "!!!" + " :)"
+#  end
+#end
+
+#Shout.yell_happily("AHHHH")
+#Shout.yell_angrily("AHHHH")
+
 module Shout
-  def self.yell_angrily(words)
-    p words + "!!!" + " :("
+  def yell_angrily(words)
+    puts words.upcase! + "!!!" + ">:("
   end
 
-  def self.yell_happily(words)
-    p words + "!!!" + " :)"
+  def yell_happily(words)
+    puts words.upcase! + "!!!" + ":D"
   end
-
 end
 
-Shout.yell_happily("AHHHH")
-Shout.yell_angrily("AHHHH")
+class Angry
+  include Shout
+end
+
+class Happy
+  include Shout
+end
+
+angry = Angry.new
+angry.yell_angrily("Hello enemy")
+
+happy = Happy.new
+happy.yell_happily("Hello friend")
