752b0232ae5fa9c48930787604ec88700bf7c69e
omgannie <annie_oh@icloud.com>
2016-07-31 14:08:09 -0500

    Release 0 complete initialize, speak, and eat cookie methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,1 +1,20 @@
 # Release 0
+
+class Santa
+  def initalize
+    puts "Initializing Santa instance..."
+  end
+
+  def speak
+    puts "Ho ho ho! Haaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie_type)
+    puts "That was a good #{cookie_type} cookie!"
+  end
+end
+
+# Driver Code
+first_santa = Santa.new
+first_santa.speak
+first_santa.eat_milk_and_cookies("chocolate chip")
