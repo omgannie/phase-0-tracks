fe12eec8ea341646b5279a05864b64fe0ea03463
omgannie <annie_oh@icloud.com>
2016-08-01 11:53:49 -0500

    Solve game algorithm based on parameters, test and build spec

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -4,24 +4,26 @@ class GuessWord
   attr_accessor :already_guessed
 
   def initialize
-    puts "Initializing word to guess..."
-    @words = ["hello", "butter", "fire", "picture", "cat", "table", "glass", "phone", "strawberry"]
-    @word_to_guess = @words[5]
+    puts "Initializing guess word game..."
+    @word = ""
     @guess_count = 0
     @already_guessed = []
     @is_over = false
   end
 
-# method 1 - randomize words
-  def randomize
-    @words.shuffle!
+# method 1 - user 1 enters word to guess
+  def word_to_guess
+    puts "User 1, please enter word to guess for user 2."
+    @word = gets.chomp
+
+    @word
   end
 
-# method 2 - accept user input
+# method 2 - accept user 2 input
 # to check if guess is correct
   def check_input(guess)
     @guess_count += 1
-    if guess == @word_to_guess
+    if guess == @word
       @is_over = true
     else
       @is_over = false
@@ -32,31 +34,47 @@ class GuessWord
 # method 3 - player receives feedback on current state of word
 # produce letter hints if guess for letter hint is right
   def hint(guess)
-    guess.split('')
+    array1 = guess.chars.to_a
+    array2 = @word.chars.to_a
 
-    guess.each do |letter|
-      if @word_to_guess.include?(letter)
-        p "Your word includes '#{letter}'!"
+    array1.each do |letter|
+      if array2.include?(letter)
+        unless array2.count(letter) > 1
+          p "Your word includes '#{letter}'!"
+        end
+      elsif array1 & array2 == array2
+        @is_over = true
+        break
       end
     end
+  end
 
-    guess.join('')
+  # method 4 - return number of guesses left
+  def guesses_left
+    @word.length - @guess_count
+  end
+
+# method 5 - feedback for repeat guesses
+  def check_repeat(guess)
+    if @already_guessed.include?(guess) && @already_guessed.count(guess) > 1
+      @guess_count -= 1
+      p "You have already guessed this word. You have #{guesses_left} guesses left."
+    end
   end
 
 end
 
 # user interface
-# input: a word
-# output: attempts to guess the word
+# input: user 1 - a word
 
 new_game = GuessWord.new
-new_game.randomize
+word = new_game.word_to_guess
+
+# input: user 2 - guesses
 
 # loop to handle repeat guesses
 # repeated guesses to not count against user
-
-# congratulatory message if they win
-# taunting method if lose
+# with output: feedback based on attempts to guess the word
 
 while !new_game.is_over
   puts "Guess the word!"
@@ -64,7 +82,16 @@ while !new_game.is_over
 
   new_game.check_input(guess)
   new_game.hint(guess)
+  new_game.guesses_left
+  new_game.check_repeat(guess)
+
+  if guess != word && (new_game.guesses_left == 0)
+    p "You ran out of guesses! HAHA LOSER!"
+    is_over = true
+    break
+  elsif guess == word && (new_game.guesses_left >= 0)
+    p "Congraulations on guessing the word!"
+    is_over = true
+  end
 
 end
-
-p "Congraulations on guessing the word!"
--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -3,16 +3,28 @@ require_relative 'game'
 describe GuessWord do
   let(:new_game) { GuessWord.new }
 
-  it "resets game & generates random word on initialization" do
-    expect(new_game.randomize).to eq "#{@word_to_guess}"
+  it "resets guess count on initialization, prompts user 1 to enter word for user 2 to guess" do
+    new_game.word_to_guess
+    expect(new_game.word_to_guess).to eq @word
   end
 
   it "takes user input and checks guess to see if correct" do
-    expect(new_game.check_input(guess)).to eq "#{@is_over}"
+    new_game.check_input(guess)
+    expect(new_game.check_input(guess)).to eq @is_over
   end
 
   it "gives user hint on letter guesses" do
+    new_game.hint(guess)
     expect(new_game.hint(guess)).to eq "Your word includes '#{letter}'!"
   end
 
+  it "returns guesses user has left" do
+    new_game.guesses_left
+    expect(new_game.guesses_left).to eq @word.length - @guess_count
+  end
+
+  it "checks for repeat guesses" do
+    new_game.check_repeat(guess)
+    expect(new_game.check_repeat(guess)).to eq @guess_count
+  end
 end
