4babce4c8a5b2b52e09d26e1821d1822eb3a7e3a
omgannie <annie_oh@icloud.com>
2016-08-14 18:49:39 -0500

    Fix SQL errors

--- databases/coffee_shop/coffee_shop.rb
+++ databases/coffee_shop/coffee_shop.rb
@@ -4,10 +4,8 @@
 require 'sqlite3'
 
 # create SQLite3 database
-users_db = SQLite3::Database.new("users.db")
-history_db = SQLite3::Database.new("order_history.db")
-users_db.results_as_hash = true
-history_db.results_as_hash = true
+db = SQLite3::Database.new("orders.db")
+db.results_as_hash = true
 
 # string delimiters
 # create table command for users
@@ -26,20 +24,21 @@ SQL
 
 # create table command for order history
 create_history_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS order_history (
+  CREATE TABLE IF NOT EXISTS order_history(
     id INTEGER PRIMARY KEY,
-    order VARCHAR(255),
-    ordered_when DATETIME NOW(),
+    item VARCHAR(255),
     user_id INT,
     FOREIGN KEY (user_id) REFERENCES users(id)
   )
 SQL
 
-# create tables
-users_db.execute(create_users_table_cmd)
-history_db.execute(create_history_table_cmd)
+# create tables in db files
+db.execute(create_users_table_cmd)
+db.execute(create_history_table_cmd)
 
 # add test user & order
-users_db.execute("INSERT INTO users(name, email, address, city, state, zipcode, card_info) VALUES('Annie Oh', 'omgannie@gmail.com', '1234 Colorful Rd.', 'Chicago', 'IL', 60606, 0000-0000-0000-0000)")
+db.execute("INSERT INTO order_history(item, user_id) VALUES('Cappucino', 1)")
+db.execute("INSERT INTO users(name, email, address, city, state, zipcode, card_info)
+VALUES('Annie Oh', 'omgannie@gmail.com', '1234 Colorful Rd.', 'Chicago', 'IL', 60606, 0000000000000000)")
 
 # user interface
