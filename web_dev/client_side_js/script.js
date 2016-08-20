c82ebde06001c2d9a20028ffe2289f9dedda1da7
omgannie <annie_oh@icloud.com>
2016-08-20 15:01:47 -0500

    Complete challenge, manipulate DOM based on user event

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,5 +1,19 @@
 console.log("Script.js is running!");
 
-var title = document.getElementsByTagName("header");
-
+var titles = document.getElementsByTagName("header");
+var title = titles[0]
 title.style.border = "5px dotted pink";
+
+function hideImageOnClick(event) {
+  event.target.hidden = true;
+}
+
+var pic = document.getElementById("kitten-pic");
+pic.addEventListener("click", hideImageOnClick);
+
+function changeFontColor(event) {
+  event.target.style.color = "orange";
+}
+
+var els = document.getElementsByTagName("h1");
+els.addEventListener("click", changeFontColor);
